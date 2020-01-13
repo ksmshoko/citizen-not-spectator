b7e34d82ea740ac6bdf8916972382c5f8138502b
ksmshoko <ksmshoko@gmail.com>
2020-01-13 22:53:10 +0200

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function() {
-//     $('input#input_text, textarea#textarea2').characterCounter();
-//     $(".dropdown-trigger").dropdown();
-//   });
-
-$(document).ready(function(){
-   $('.modal').modal();
-   $('.dropdown-trigger').dropdown();
-});
